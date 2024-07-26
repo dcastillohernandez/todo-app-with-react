9ab49c9c358906627d3c2f8c2525bfeaac90b45c
dayronCastilloH <thecodedevcompany0@gmail.com>
2024-07-26 00:29:02 -0600

    add TodoItem Component

--- src/useReducer/TodoList.jsx
+++ src/useReducer/TodoList.jsx
@@ -1,22 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { TodoItem } from './TodoItem';
 
-export const TodoList = ({ todos }) => {
+export const TodoList = ({ todos = [] }) => {
     return (
         <ul className="list-group">
             {
                 todos.map(todo => (
-                    <li
-                        key={todo.id}
-                        className="list-group-item d-flex justify-content-between">
-                        <span
-                            className="align-self-center">
-                            Item 1
-                        </span>
-                        <button
-                            className="btn btn-danger">
-                            Borrar
-                        </button>
-                    </li>
+                    <TodoItem key={todo.id} todo={todo}/>
                 ))
             }
         </ul>
