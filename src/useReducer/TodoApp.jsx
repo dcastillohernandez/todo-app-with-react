6677141808d53cd6f35915d96c8f87a860c938b5
dayronCastilloH <thecodedevcompany0@gmail.com>
2024-07-26 00:15:19 -0600

    update delete space

--- src/useReducer/TodoApp.jsx
+++ src/useReducer/TodoApp.jsx
@@ -42,7 +42,6 @@ export const TodoApp = () => {
                                 </li>
                             ))
                         }
-
                     </ul>
                 </div>
 
