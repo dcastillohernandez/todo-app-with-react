4d03fbcac33a1f649ed501246d7775ebf6a1fe86
dayronCastilloH <thecodedevcompany0@gmail.com>
2024-07-25 20:19:21 -0600

    update

--- src/todo-app/TodoApp.jsx
+++ src/todo-app/TodoApp.jsx
@@ -9,7 +9,7 @@ const initialState = [
     },
     {
         id: new Date().getTime() * 3,
-        description: 'Recolectar la piedra del Alma',
+        description: 'Recolectar la piedra del tiempo',
         done: false
     }
 ]
@@ -17,38 +17,65 @@ const initialState = [
 export const TodoApp = () => {
 
     const [todos, dispatch] = useReducer(TodoReducer, initialState)
+
+    const handleNewTodo = ( todo ) => {
+        console.log({ todo });
+    }
+
     return (
         <>
-            <h1>TodoApp</h1>
+            <h1>TodoApp: 10, <small>pendientes: 2</small></h1>
             <hr />
 
             <div className="row">
                 <div className="col-7">
-                    <ul>
-                        <li>Items 1</li>
-                        <li>Items 2</li>
-                        <li>Items 3</li>
+
+                    {/* TodoList */}
+                    <ul className="list-group">
+                        {
+                            todos.map(todo => (
+                                // TodoItem...
+                                <li
+                                    key={todo.id}
+                                    className="list-group-item d-flex justify-content-between">
+                                    <span
+                                        className="align-self-center">
+                                        Items 1
+                                    </span>
+                                    <button
+                                        className="btn btn-danger">
+                                        Borrar
+                                    </button>
+                                </li>
+                            ))
+                        }
                     </ul>
+                    {/* Fin TodoList */}
                 </div>
 
                 <div className="col-5">
                     <h4>Agregar Todo</h4>
                     <hr />
-
+                    {/* TodoAdd onNewTodo( todo ) */}
+                    {/* { id: new Date()..., description: '', done: false } */}
                     <form>
                         <input
                             type="text"
                             placeholder="¿Que hay que hacer?"
                             className="form-control"
                         />
+
+                        <button
+                            type="submit"
+                            className="btn btn-outline-primary mt-1 "
+                        >
+                            Agregar
+                        </button>
                     </form>
+                    {/* Fin TodoAdd */}
                 </div>
+
             </div>
-
-
-
-
-
         </>
     )
 }
